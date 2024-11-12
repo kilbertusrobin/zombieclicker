4053a66cb58c811718b36d8a9d0e0a27f3f3356d
Matis Rault <matis.rault@ynov.com>
2024-11-12 10:35:46 +0100

    app empty

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -5,7 +5,9 @@ const Layout = () => {
   return (
     <div className="flex h-full w-full">
       <div className="flex-1 bg-blue-500 h-full"></div>
-      <div className="flex-[2_2_0%] bg-[url('/img/hero-pattern.svg')] h-full"></div>
+      <div className="flex-[2_2_0%] bg-[url('/assets/zombie-bg.webp')] bg-cover h-full">
+      <img src="" alt="" />
+      </div>
       <div className="flex-1 bg-green-500 h-full"></div>
     </div>
   );
