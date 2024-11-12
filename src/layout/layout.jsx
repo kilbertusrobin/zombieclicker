98508749af8d4ca700ff879029fc508c0e3f19b3
Matis Rault <matis.rault@ynov.com>
2024-11-12 11:27:53 +0100

    fix layout

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -6,7 +6,9 @@ const Layout = () => {
 
   return (
     <div className=" relative flex h-full w-full">
-      <div className="flex-1 bg-blue-500 h-full"></div>
+      <div className="flex-1 bg-blue-500 h-full">
+        <UpgradeCard name="Soldat" image="/assets/cursor.png" price="100" quantity="1"/>
+      </div>
         <div className="flex-[2_2_0%] bg-[url('/assets/zombie-bg.webp')] bg-cover h-full">
             <ZombieContainer />
         </div>
@@ -17,7 +19,6 @@ const Layout = () => {
         <div className='w-full h-1/2'></div>
       </div>
       <div className='w-48 h-48 rounded-lg absolute border top-36 left-[38rem] border-red-500'>
-x      <UpgradeCard name="Soldat" image="/assets/cursor.png" price="100" quantity="1"/>
       </div>
     </div>
   );
