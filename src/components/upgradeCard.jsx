2eadd39b8c7a2be6f96320b80b7c3d94323651ce
Matis Rault <matis.rault@ynov.com>
2024-11-12 12:00:57 +0100

    add: icons

--- src/components/upgradeCard.jsx
+++ src/components/upgradeCard.jsx
@@ -13,14 +13,14 @@ const UpgradeCard = ({ name, image, initialPrice, initialQuantity }) => {
         <div onClick={handleClick} className="flex flex-row items-center border justify-between px-6 py-7 cursor-pointer">
             <div className="flex flex-row gap-6 items-center">
                 <div className="flex flex-col">
-                    <img src={image} alt={name} />
+                    <img className='w-16 h-full' src={image} alt={name} />
                 </div>
                 <div className="flex flex-col">
-                    <h2>{name}</h2>
+                    <h2 className='text-2xl'>{name}</h2>
                     <p>Price: {price.toFixed(2)} Headshot</p>
                 </div>
             </div>
-            <p>Quantity: {quantity}</p>
+            <p className='text-2xl'>{quantity}</p>
         </div>
     );
 };
