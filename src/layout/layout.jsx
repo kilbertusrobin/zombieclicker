bfeaf9aedb0a42f866ca60fa41eacc7d06914996
Matis Rault <matis.rault@ynov.com>
2024-11-12 10:27:32 +0100

    change Layout

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Layout = () => {
 
   return (
-    <div>
+    <div className="flex h-full w-full">
+      <div className="flex-1 bg-blue-500 h-full"></div>
+      <div className="flex-[2_2_0%] bg-[url('/img/hero-pattern.svg')] h-full"></div>
+      <div className="flex-1 bg-green-500 h-full"></div>
     </div>
   );
 };
