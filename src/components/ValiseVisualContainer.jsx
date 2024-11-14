6b51e8a9345f2eefc2036963694e3cb16677159a
RobinK <robin.kilbertus@gmail.com>
2024-11-14 16:22:18 +0100

    merge

--- src/components/ValiseVisualContainer.jsx
+++ src/components/ValiseVisualContainer.jsx
@@ -1,4 +1,16 @@
 const ValiseVisualContainer = ({ speed, distance }) => {
+
+    let gifKm = 0;
+    if (speed<20) {
+        gifKm = 1; 
+    } else if (speed>20 && speed<30) {
+        gifKm = 20;
+    } else if (speed>30 && speed<40) {
+        gifKm = 30;
+    } else {
+        gifKm = 40;
+    }
+
     return (
         <div className="w-full h-full relative">
             <h1 className="text-4xl text-center font-bold text-black absolute top-10 w-full z-30">
@@ -9,7 +21,7 @@ const ValiseVisualContainer = ({ speed, distance }) => {
             </h1>
             
             <img src="./assets/bg-valise.gif" className="w-full h-full object-cover absolute mb-40" alt="valise" />
-            <img src="./assets/road40kmh.gif" className="w-full h-1/2 object-cover absolute bottom-0" alt="road" />
+            <img src={`./assets/road${gifKm}kmh.gif`} className="w-full h-1/2 object-cover absolute bottom-0" alt="road" />
             <img
                 src="./assets/valisemobile.png"
                 className="h-1/3 w-auto absolute bottom-5 left-0 right-0 m-auto z-10"
