2af93f81f67a33e63ec13f7fac65808186b63319
Matis Rault <matis.rault@ynov.com>
2024-11-14 11:35:58 +0100

    plus lisible

--- src/components/AllUpgradeCards.jsx
+++ src/components/AllUpgradeCards.jsx
@@ -1,20 +1,94 @@
-import React, {useCallback } from 'react';
+import React, { useCallback } from 'react';
 import UpgradeCard from "./upgradeCard";
 
 const AllUpgradeCards = ({ score, setScore, updateUpgradeQuantity }) => {
-    
+  return (
+    <>
+      <UpgradeCard
+        name="Brute"
+        image="/assets/batte.svg"
+        initialPrice={50}
+        initialQuantity={0}
+        incomeRate={1}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(0, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_0"
+      />
 
-    return (
-        <>
-            <UpgradeCard name="Brute" image="/assets/batte.svg" initialPrice={50} initialQuantity={0} incomeRate={1} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(0, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_0" />
-            <UpgradeCard name="Pistolero" image="/assets/pistolet.svg" initialPrice={500} initialQuantity={0} incomeRate={10} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(1, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_1" />
-            <UpgradeCard name="Soldat" image="/assets/fusil.svg" initialPrice={2500} initialQuantity={0} incomeRate={50} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(2, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_2" />
-            <UpgradeCard name="Commando" image="/assets/commando.svg" initialPrice={5000} initialQuantity={0} incomeRate={100} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(3, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_3" />
-            <UpgradeCard name="Blindé" image="/assets/tank.svg" initialPrice={10000} initialQuantity={0} incomeRate={250} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(4, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_4" />
-            <UpgradeCard name="Bombardier" image="/assets/bombardier.svg" initialPrice={50000} initialQuantity={0} incomeRate={500} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(5, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_5" />
-            <UpgradeCard name="Annihilateur" image="/assets/annihilateur.svg" initialPrice={100000} initialQuantity={0} incomeRate={1000} score={score} setScore={setScore} updateQuantity={useCallback((quantity) => updateUpgradeQuantity(6, quantity), [updateUpgradeQuantity])} upgradeKey="upgrade_6" />
-        </>
-    );
+      <UpgradeCard
+        name="Pistolero"
+        image="/assets/pistolet.svg"
+        initialPrice={500}
+        initialQuantity={0}
+        incomeRate={10}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(1, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_1"
+      />
+
+      <UpgradeCard
+        name="Soldat"
+        image="/assets/fusil.svg"
+        initialPrice={2500}
+        initialQuantity={0}
+        incomeRate={50}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(2, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_2"
+      />
+
+      <UpgradeCard
+        name="Commando"
+        image="/assets/commando.svg"
+        initialPrice={5000}
+        initialQuantity={0}
+        incomeRate={100}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(3, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_3"
+      />
+
+      <UpgradeCard
+        name="Blindé"
+        image="/assets/tank.svg"
+        initialPrice={10000}
+        initialQuantity={0}
+        incomeRate={250}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(4, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_4"
+      />
+
+      <UpgradeCard
+        name="Bombardier"
+        image="/assets/bombardier.svg"
+        initialPrice={50000}
+        initialQuantity={0}
+        incomeRate={500}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(5, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_5"
+      />
+
+      <UpgradeCard
+        name="Annihilateur"
+        image="/assets/annihilateur.svg"
+        initialPrice={100000}
+        initialQuantity={0}
+        incomeRate={1000}
+        score={score}
+        setScore={setScore}
+        updateQuantity={useCallback((quantity) => updateUpgradeQuantity(6, quantity), [updateUpgradeQuantity])}
+        upgradeKey="upgrade_6"
+      />
+    </>
+  );
 };
 
 export default AllUpgradeCards;
